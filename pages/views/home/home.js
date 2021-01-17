e6cca73cd5eb22f6844995f415af8169b840285d
hh <huangh0305@163.com>
2021-01-17 21:14:27 +0800

    首页页面完成

--- pages/views/home/home.js
+++ pages/views/home/home.js
@@ -13,18 +13,10 @@ Page({
    */
   onLoad: function (options) {
   },
-    function(){},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
   },
   titleClick: function (e) { 
@@ -32,6 +24,34 @@ Page({
       currentIndex:e.target.dataset.index
     })
   },
+  code: function () {
+    wx.scanCode({
+    //  是否只能从相机扫码，不允许从相册选择图片
+      onlyFromCamera: false,
+      // 扫码类型
+      scanType: ['qrCode', 'barCode', 'datamatrix', 'pdf417'],
+      // 接口调用成功的回调函数
+      success: (result)=>{
+        console.log(result)
+      },
+      // 接口调用失败的回调函数
+      fail: () => { 
+        // wx.showToast({
+        //   title: '未成功',
+        //   duration: 2000
+        // })
+      },
+      // 接口调用结束的回调函数（调用成功、失败都会执行）
+      complete: ()=>{}
+    });
+   },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
   /**
    * 生命周期函数--监听页面隐藏
    */
